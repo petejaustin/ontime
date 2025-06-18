1f64e66c773b78247dd9071afa3611a2404ba612
Patrick Totzke <patrick@totzke.uk>
2025-06-18 13:10:56 +0100

    parser cleanup

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
+
 use lalrpop_util::lalrpop_mod;
-use crate::temporal_graphs::NodeAttr;
+use crate::temporal_graphs::{Node, NodeAttr, Edge, TemporalGraph};
 
 
 #[derive(Debug)]
@@ -11,3 +13,62 @@ pub enum ParsedLine {
 
 lalrpop_mod!(pub tg_parser, "/parser/tg_parser.rs"); // LALRPOP parser module
 //lalrpop_mod!(formula_parser);
+
+
+pub fn temporal_graph_from_lines(lines: Vec<ParsedLine>) -> TemporalGraph {
+        // first collect all nodes and edges
+        let mut node_lines = Vec::new();
+        let mut edge_lines = Vec::new();
+        for item in lines {
+            match item {
+                ParsedLine::Node(_,_) => node_lines.push(item),
+                ParsedLine::Edge(_,_,_) => edge_lines.push(item),
+                ParsedLine::Empty => {},
+            }
+        }
+
+        // Map string node IDs to indices
+        let mut id_map = HashMap::new();
+        let mut node_attrs: HashMap<Node, HashMap<String,NodeAttr>> = HashMap::new();
+        let mut next_idx = 0;
+
+
+        for item in &node_lines {
+            if let ParsedLine::Node(id, attrs) = item {
+                let idx = *id_map.entry(id.clone()).or_insert_with(|| {
+                    let i = next_idx;
+                    next_idx += 1;
+                    i
+                });
+
+                let mut attr_map = HashMap::<String, NodeAttr>::new();
+
+                for a in attrs {
+                    match a {
+                        NodeAttr::Owner(_) => {attr_map.insert("owner".to_string(), a.clone());}
+                        NodeAttr::Label(_) => {attr_map.insert("label".to_string(), a.clone());}
+                    }
+                }
+                node_attrs.insert(idx,attr_map);
+            }
+        }
+
+        let node_count = next_idx;
+
+        let mut edges = Vec::new();
+
+        for item in &edge_lines {
+            if let ParsedLine::Edge(from_id, to_id, attr) = item {
+                let from = *id_map.get(from_id).unwrap();
+                let to = *id_map.get(to_id).unwrap();
+                let available_at = |_: usize| true;
+                edges.push(Edge::new(from, to, available_at));
+            }
+        }
+
+        TemporalGraph::new(
+            node_count,
+            node_attrs,
+            edges,
+        )
+    }
